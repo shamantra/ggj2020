586915a43c65d6390721b746e75db94931062e29
Bhavesh Patel <bhavesh.patel@algorintechlabs.com>
2020-02-01 13:55:33 +0530

    Adding json parser for level.

---
+++ Assets/Scripts/JSONReader.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class JSONReader : MonoBehaviour
+{
+    public TextAsset jsonFile;
+
+    void Start()
+    {
+        Layout level = JsonUtility.FromJson<Layout>(jsonFile.text);
+        print(level);
+        //foreach (Employee employee in employeesInJson.employees)
+        //{
+        //    Debug.Log("Found employee: " + employee.firstName + " " + employee.lastName);
+        //}
+    }
+}
---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class Layout
+{
+    public int level;
+    public Block[] blocks;
+}
+
+[Serializable]
+public class Block
+{
+    public int type;
+    public Vector2 position;
+    public int answerRotation;
+    public int initialRotation;
+}
