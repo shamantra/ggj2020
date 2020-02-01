c1efb9cab157c03cee08c18e62b88d6f00b3500e
Bhavesh Patel <bhavesh.patel@algorintechlabs.com>
2020-02-01 19:29:50 +0530

    Adding level change methods and level files.

---
+++ Assets/Scripts/LevelReader.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class LevelReader : MonoBehaviour
+{
+    int level;
+    Layout currentLevel;
+
+    private void Awake()
+    {
+        LoadLevel(++level);
+    }
+
+    public Layout LoadNextLevel()
+    {
+        return LoadLevel(level++);
+    }
+
+    Layout LoadLevel(int lvl)
+    {
+        TextAsset jsonFile = Resources.Load("level" + lvl) as TextAsset;
+
+        level = lvl;
+        currentLevel = JsonUtility.FromJson<Layout>(jsonFile.text);
+        GameLevel.getGameLevel().setLevel(currentLevel);
+
+        return currentLevel;
+    }
+
+    void Start()
+    {
+    }
+}
